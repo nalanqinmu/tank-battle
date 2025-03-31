63c56d876d873b5f7ef5fccaa50c6eefef8f1432
zhaoyanda <1758259198@qq.com>
2025-03-31 16:08:14 +0800

    feat: 新增

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,22 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/',
   resolve: {
     alias: {
       '@': '/src'
+    }
+  },
+  build: {
+    assetsInlineLimit: 4096,
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          three: ['three', 'three/examples/jsm/controls/OrbitControls.js'],
+          physics: ['cannon-es']
+        }
+      }
     }
   }
 })
